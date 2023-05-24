d4fa36dfcfaf51a7777f40e4f513709da957b893
EC2 Default User <ec2-user@ip-172-31-37-17.ap-northeast-1.compute.internal>
2023-05-24 11:51:59 +0000

    just practicing1

--- app/javascript/packs/homes/top.js
+++ app/javascript/packs/homes/top.js
@@ -1,1 +1,30 @@
 alert('Hello World');
+let hello = 'Hello World';
+alert(hello);
+alert(4 + 3)
+alert('Hello' + 'World')
+let str1 = 'Hi';
+let str2 = 'Ocean'
+alert(str1 + str2)
+
+let orange = 100;
+let apple = 120;
+
+if (orange < apple){
+  alert('みかんの値段がリンゴより安い');
+} else if(orange == apple){
+  alert('みかんの値段がりんごと同じ')
+} else {
+  alert('みかんの値段がりんごより高い')
+}
+
+let max = 100;
+let num = 1;
+let count = 0;
+
+while(num < max){
+  num = num * 2;
+  count = count + 1;
+}
+
+alert('2を掛けて' + max + 'を超えるのに必要だった回数は' + count + '回です')；
