1f9ad803beff27b64ae1212b302a18ce85569625
EC2 Default User <ec2-user@ip-172-31-37-17.ap-northeast-1.compute.internal>
2023-05-27 09:44:50 +0000

    just practicing4

--- app/javascript/packs/homes/top.js
+++ app/javascript/packs/homes/top.js
@@ -42,3 +42,17 @@ alert(30 + 3)
 alert(33 - 22)
 alert(22 * 1.5)
 alert(33 / 22)
+
+let alertString;
+alertString = addString("WebCamp");
+alert(alertString);
+function addString(strA){
+  let addStr = "Hello" + strA;
+  return addStr;
+}
+
+let promptStr = prompt('何か好きな文字を入力してください。');
+alert(promptStr);
+
+let user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
+alert('あなたの選んだ手は' + user_hand + 'です。');
